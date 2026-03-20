a96e83e26605ed28ea3dd57fcc61e9c5a0bc3e6f
KuBin-dot28 <leq08247@gmail.com>
2026-03-20 20:41:53 +0700

    package.js

---
+++ package.js
@@ -0,0 +1,14 @@
+{
+  "name": "kubindev-api",
+  "version": "1.0.0",
+  "description": "API Phân tích dữ liệu",
+  "main": "1.js",
+  "scripts": {
+    "start": "node 1.js"
+  },
+  "dependencies": {
+    "ws": "^8.13.0",
+    "express": "^4.18.2",
+    "cors": "^2.8.5"
+  }
+}
